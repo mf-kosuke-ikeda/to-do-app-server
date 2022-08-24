43e96a99d8cec8dca19d0411320b1f2adbfbfdf8
kazato_yamaguchi <kazato_yamaguchi@mforce.co.jp>
2022-08-24 21:07:38 +0900

    importのモジュール名修正とprismaserviceの提供

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserService } from './user/user.service';
+import { UsersService } from './user/user.service';
 import { CommentService } from './comment/comment.service';
 import { TaskService } from './task/task.service';
-import { UserController } from './user/user.controller';
+import { UsersController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import { TaskModule } from './task/task.module';
 import { CommentModule } from './comment/comment.module';
+import {PrismaService} from './prisma.service'
 
 @Module({
   imports: [UserModule, TaskModule, CommentModule],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService, CommentService, TaskService],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService, CommentService, TaskService, PrismaService],
 })
 export class AppModule {}
